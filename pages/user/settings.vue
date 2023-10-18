c2bcdf297fc8d99263b0805190a5ea9b2e4f6480
Saishravan Muthukrishnan <saishravanm@gmail.com>
2023-10-18 16:06:37 -0500

    yeah

--- pages/user/settings.vue
+++ pages/user/settings.vue
@@ -5,6 +5,13 @@ const eventData = events.data
 </script>
 
 <template>
+<<<<<<< Updated upstream
+=======
+  <div class="w-[426px] h-[1219px] relative"></div>
+  <div class="w-[426px] h-[1219px] left-0 top-0 absolute bg-white"></div>
+  <div class="w-[426px] h-[1219px] left-0 top-0 absolute bg-white"></div>
+  <div class="w-[352.90px] h-[834.76px] left-[37px] top-[62px] absolute"></div>
+>>>>>>> Stashed changes
   <h1><u>Account Information</u></h1>
   <h3>{{ data?.email }}</h3>
   <h3>{{ data?.name }}</h3>
