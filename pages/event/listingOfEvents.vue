79c2a09c2c1cfec69bcc1a74c989befee036a1b5
Hassan H <hkacperh@gmail.com>
2023-10-23 22:46:35 -0500

    Co-authored-by: nmaraboyina <nmaraboyina@users.noreply.github.com> Co-authored-by: Elisa Paul <elisap15@users.noreply.github.com>

---
+++ pages/event/listingOfEvents.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+
+// Fetch API call
+const { events } = await useFetch('/api/events/')
+const eventData = events.data
+
+// Import Prisma client
+import { PrismaClient } from '@prisma/client/edge'
+const prisma = new PrismaClient()
+
+</script>
+<!--Copy paste from Sai's code-->
+<template>
+<div v-if="event.id != null">
+    <h1 v-for="event in eventData" :key="event.id">
+      {{ !event.isSignUpAvailable }}
+    </h1>
+  </div>
+
+</template>
+
