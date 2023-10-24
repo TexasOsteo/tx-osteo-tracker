8b717670864e29af34211a79192fcff93e398026
Hassan H <hkacperh@gmail.com>
2023-10-23 22:46:35 -0500

    Co-authored-by: nmaraboyina <nmaraboyina@users.noreply.github.com> Co-authored-by: Elisa Paul <elisap15@users.noreply.github.com>

---
+++ components/EventListing.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <div className="h-screen w-screen bg-slate-500">
+    <div className="bg-white mx-20 p-5 rounded-md shadow-xl">
+      <div className="flex flex-wrap items-end">
+        <h1 className="text-4xl font-bold font-sans mr-10">Event Name</h1>
+        <h3 className="">Capacity</h3>
+      </div>
+      <div className="flex flex-wrap mt-5">
+        <h3 className="mr-20">Organizer</h3>
+        <h3 className="mr-20">Location</h3>
+        <h3 className="">Date and Time</h3>
+      </div>
+    </div>
+  </div>
+</template>
