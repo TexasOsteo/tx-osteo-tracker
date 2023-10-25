b4ecc0b37cd4793d4f207213420adb015961f2c1
Hassan H <hkacperh@gmail.com>
2023-10-25 18:44:32 -0500

    Displaying data correctly in EventListing

--- pages/event/listings.vue
+++ pages/event/listings.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-
 // Fetch API call by destructing the events response object into its data
 const { data: events } = await useFetch(`/api/events?after=${Date.now()}`)
 </script>
 <template>
-  <li v-for="event in events" :key="event.id">
-    <EventListing :event="event"> </EventListing>
-  </li>
+  <ul>
+    <li v-for="event in events" :key="event.id">
+      <EventListing :event="event"> </EventListing>
+    </li>
+  </ul>
 </template>
