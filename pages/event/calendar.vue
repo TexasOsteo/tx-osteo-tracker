f24baa89a8b17bc352096149121598da812f793f
htthuyen <htth0105@gmail.com>
2023-10-25 16:06:03 -0500

    Complete calendar component

---
+++ pages/event/calendar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="text-1xl font-bold">TEXAS OSTEOPROSIS FOUNDATION</h1>
+    <h2 class="text-3xl text-center">SIGN UP</h2>
+  </div>
+  <div>
+    <table class="table-fixed border-separate border-spacing-10">
+      <tr class="text-center place-content-strech self-auto">
+        <th>
+          <FormKit
+            type="select"
+            name="location"
+            label="Location"
+            :options="{
+              plano: 'Plano',
+              dallas: 'Dallas',
+              Frisco: 'Frisco',
+            }"
+          />
+        </th>
+        <th>Time</th>
+        <th>Organization</th>
+        <th>Even Category</th>
+      </tr>
+    </table>
+  </div>
+  <div><CalendarDisplay /></div>
+</template>
+
+<script setup lang="ts"></script>
