ee289249d3cccc3b9238974307b1fa0ad3f34584
Saishravan Muthukrishnan <saishravanm@gmail.com>
2023-10-25 11:11:14 -0500

    Finished base user-settings page
    Still no cards for the Event listings.

--- pages/user/settings.vue
+++ pages/user/settings.vue
@@ -45,49 +45,62 @@ const eventData = events.data
           <div
             class="w-[352.90px] h-[69.76px] left-0 top-[408px] absolute text-black text-xl font-light font-['Work Sans'] tracking-[4px]"
           ></div>
-          <div
-            class="w-[318px] h-[38px] left-[3px] top-0 absolute text-black text-xl font-light font-['Work Sans'] tracking-[4px]"
-          >
-            PREFERRED NAME:
-          </div>
         </div>
       </div>
       <div
-        class="w-[350px] h-[54px] left-[38px] top-[936px] absolute bg-white rounded-[20px] border border-teal-500"
+        class="w-[350px] h-[54px] left-[38px] top-[1100px] absolute bg-white rounded-[20px] border border-teal-500"
       ></div>
       <div
-        class="w-[350px] h-[54px] left-[36px] top-[1016px] absolute bg-teal-500 rounded-[20px]"
+        class="w-[350px] h-[54px] left-[36px] top-[1200px] absolute bg-teal-500 rounded-[20px]"
       ></div>
       <div
-        class="w-[350px] h-[54px] left-[36px] top-[1096px] absolute bg-red-600 rounded-[20px]"
+        class="w-[350px] h-[54px] left-[36px] top-[1300px] absolute bg-red-600 rounded-[20px]"
       ></div>
       <button
-        class="w-[191px] h-7 left-[115px] top-[949px] absolute text-center text-teal-500 text-xl font-light font-['Work Sans'] tracking-[4px]"
+        class="w-[191px] h-7 left-[115px] top-[1110px] absolute text-center text-teal-500 text-xl font-light font-['Work Sans'] tracking-[4px]"
       >
         EDIT ACCOUNT
       </button>
       <button
-        class="w-[191px] h-7 left-[115px] top-[1029px] absolute text-center text-white text-xl font-normal font-['Work Sans'] tracking-[4px]"
+        class="w-[191px] h-7 left-[115px] top-[1210px] absolute text-center text-white text-xl font-normal font-['Work Sans'] tracking-[4px]"
       >
         SIGN OUT
       </button>
       <button
-        class="w-[236px] h-7 left-[95px] top-[1109px] absolute text-center text-white text-xl font-normal font-['Work Sans'] tracking-[4px]"
+        class="w-[236px] h-7 left-[95px] top-[1310px] absolute text-center text-white text-xl font-normal font-['Work Sans'] tracking-[4px]"
       >
         DELETE ACCOUNT
       </button>
     </div>
   </div>
-  <h3>{{ data?.userNotes }}</h3>
-  <h3>{{ data?.qualifications }}</h3>
-  <h1><u>Upcoming Events</u></h1>
-  <div v-if="eventData != null">
-    <h1 v-for="event in eventData" :key="event.id">{{ event.name }}</h1>
+  <div
+    class="w-[352.90px] h-[69.76px] left-[40px] top-[900px] absolute text-black text-xl font-light font-['Work Sans'] tracking-[4px]"
+  >
+    USER NOTES: {{ data?.userNotes }}
   </div>
-  <h1><u>Past Events</u></h1>
-  <div v-if="eventData != null">
-    <h1 v-for="event in eventData" :key="event.id">
-      {{ !event.isSignUpAvailable }}
-    </h1>
+
+  <div
+    class="w-[352.90px] h-[69.76px] left-[40px] top-[1000px] absolute text-black text-xl font-light font-['Work Sans'] tracking-[4px]"
+  >
+    QUALIFICATIONS: {{ data?.qualifications }}
+  </div>
+
+  <div
+    class="w-[352.90px] h-[69.76px] left-[600px] top-[200px] absolute text-black text-xl font-light font-['Work Sans'] tracking-[4px]"
+  >
+    UPCOMING EVENTS:
+    <div v-if="eventData != null">
+      <h1 v-for="event in eventData" :key="event.id">{{ event.name }}</h1>
+    </div>
+  </div>
+  <div
+    class="w-[352.90px] h-[69.76px] left-[600px] top-[300px] absolute text-black text-xl font-light font-['Work Sans'] tracking-[4px]"
+  >
+    PAST EVENTS:
+    <div v-if="eventData != null">
+      <h1 v-for="event in eventData" :key="event.id">
+        {{ !event.isSignUpAvailable }}
+      </h1>
+    </div>
   </div>
 </template>
