3f24c0bd5f2e8644711039642c7bf2ba2a691886
Hassan H <hkacperh@gmail.com>
2023-10-24 23:54:44 -0500

    Did EventListing component and implementation
    Co-authored-by: Sam <md-y@users.noreply.github.com>

---
+++ pages/event/listings.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+// Fetch API call by destructing the events response object into its data
+const { data: events } = await useFetch(`/api/events?after=${Date.now()}`)
+</script>
+<template>
+  <li v-for="event in events" :key="event.id">
+    <EventListing :event="event"> </EventListing>
+  </li>
+</template>
