585465ca3111e516954f185a3fcac784b66c1dbe
Sam <15069105+md-y@users.noreply.github.com>
2023-11-08 12:14:50 -0600

    Create OsteoLogo.vue

---
+++ components/OsteoLogo.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="https://static.wixstatic.com/media/8c1f13_bd14a68191934239a53edefd6e80f734~mv2.png/v1/fill/w_204,h_204,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logo_whitecircle.png" />
+</template>
