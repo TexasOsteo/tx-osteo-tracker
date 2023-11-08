e03bd34c23f28dadf2afb364231f7a081b58109a
Sam <15069105+md-y@users.noreply.github.com>
2023-11-08 12:16:05 -0600

    Create EventList.vue

---
+++ components/EventList.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      isExpanded: false,
+    }
+  },
+  methods: {
+    toggleExpanded() {
+      this.isExpanded = !this.isExpanded
+    },
+  },
+}
+</script>
+
+<template>
+  <EventListing />
+</template>
