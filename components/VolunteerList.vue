ef4dd90c9573a3b5aa6f682bf62ab1c268bc0ad7
htthuyen <htth0105@gmail.com>
2023-11-16 12:19:49 -0600

    Finished styling

--- components/VolunteerList.vue
+++ components/VolunteerList.vue
@@ -29,11 +29,12 @@ const removeUser = (userId: any) => {
   isConfirmationModalOpen.value = true
   userIdToRemove.value = userId
 }
+// cancel button
 const cancelRemoval = () => {
   isConfirmationModalOpen.value = false
   userIdToRemove.value = null
 }
-// to remove volunteers
+// volunteer removal button and remove signed up user from the event
 const confirmRemoval = async () => {
   try {
     await useFetch(
@@ -55,53 +56,60 @@ const confirmRemoval = async () => {
 
 <template>
   <div v-if="isAdmin">
-    <div><h1 class="text-center text-2xl">VOLUNTEER LIST</h1></div>
-    <ul>
-      <li v-for="user in volunteerList" :key="user.id">
-        <div class="box-border h-10 w-full p-2 border-4 flex mb-1">
-          <div class="flex grid-rows-4 gap-20">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17.7549 13.9999C18.0502 14 18.3427 14.0583 18.6155 14.1715C18.8883 14.2846 19.1362 14.4504 19.3449 14.6593C19.5536 14.8682 19.7192 15.1162 19.8321 15.3892C19.945 15.6621 20.003 15.9545 20.0029 16.2499V17.1679C20.0029 17.7409 19.8239 18.2996 19.4909 18.7659C17.9449 20.9299 15.4209 22.0009 12.0009 22.0009C8.57891 22.0009 6.05591 20.9289 4.51391 18.7649C4.18203 18.2989 4.00376 17.741 4.00391 17.1689V16.2489C4.00391 15.6524 4.24085 15.0804 4.66262 14.6586C5.08439 14.2368 5.65643 13.9999 6.25291 13.9999H17.7549ZM11.9999 2.00488C12.6565 2.00488 13.3067 2.13421 13.9133 2.38548C14.52 2.63676 15.0711 3.00506 15.5354 3.46935C15.9997 3.93364 16.368 4.48484 16.6193 5.09147C16.8706 5.69809 16.9999 6.34827 16.9999 7.00488C16.9999 7.66149 16.8706 8.31167 16.6193 8.9183C16.368 9.52493 15.9997 10.0761 15.5354 10.5404C15.0711 11.0047 14.52 11.373 13.9133 11.6243C13.3067 11.8756 12.6565 12.0049 11.9999 12.0049C10.6738 12.0049 9.40205 11.4781 8.46437 10.5404C7.52669 9.60273 6.99991 8.33097 6.99991 7.00488C6.99991 5.6788 7.52669 4.40703 8.46437 3.46935C9.40205 2.53167 10.6738 2.00488 11.9999 2.00488Z"
-                fill="grey"
-              />
-            </svg>
-            <table className="table-auto">
-              <tbody>
-                <tr>
-                  <td class="w-80">{{ user.name }}</td>
-                  <td class="w-80">{{ user.email }}</td>
-                  <td class="w-80">
-                    <button @click="removeUser(user.id)">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="red"
-                        class="w-6 h-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M6 18L18 6M6 6l12 12"
-                          fill="red"
-                        />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </li>
-    </ul>
+    <div><h1 class="text-center text-2xl mb-5 mt-5">VOLUNTEER LIST</h1></div>
+
+    <div class="relative overflow-x-auto sm:rounded-lg">
+      <table
+        class="w-full text-sm text-left rtl:text-right text-black dark:text-black"
+      >
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-500 dark:bg-gray-500 dark:text-gray-800"
+        >
+          <tr>
+            <th class="px-6 py-3">Name</th>
+            <th class="px-6 py-3">Email</th>
+            <th class="px-6 py-3">Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr
+            v-for="user in volunteerList"
+            :key="user.id"
+            class="bg-white border-b dark:bg-white-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+          >
+            <td class="px-6 py-3">
+              {{ user.name }}
+            </td>
+
+            <td class="px-6 py-3">
+              {{ user.email }}
+            </td>
+
+            <td class="px-6 py-3">
+              <div>
+                <button @click="removeUser(user.id)">
+                  <svg
+                    class="h-8 w-8 text-red-500"
+                    viewBox="0 0 24 24"
+                    fill="red"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+                    <line x1="9" y1="9" x2="15" y2="15" />
+                    <line x1="15" y1="9" x2="9" y2="15" />
+                  </svg>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
     <div
       v-if="isConfirmationModalOpen"
       data-modal-backdrop="static"
@@ -123,21 +131,41 @@ const confirmRemoval = async () => {
         <div
           class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
         >
-          <div class="modal-content">
-            <p>Are you sure to delete this user?</p>
+          <div class="p-4 md:p-5 text-center">
+            <svg
+              class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <h3 class="mb-5 text-lg font-normal text-black">
+              Are you sure to delete this user?
+            </h3>
+            <h5 class="mb-5 font-normal text-gray-500">
+              "This action cannot be undone."
+            </h5>
             <div>
               <div
-                class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
+                class="flex place-content-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
               >
                 <button
                   class="text-gray-500 bg-white hover:bg-white-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                   @click="cancelRemoval"
                 >
-                  Cancel
+                  No,cancel
                 </button>
 
                 <button
-                  class="text-gray-500 bg-white hover:bg-white-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-blue-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                  class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2"
                   @click="confirmRemoval"
                 >
                   Confirm
