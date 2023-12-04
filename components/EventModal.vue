a21f75810943bbc8c9181800788639e93ec77d86
nmaraboyina <112922058+nmaraboyina@users.noreply.github.com>
2023-12-03 22:04:43 -0600

    update 4

--- components/EventModal.vue
+++ components/EventModal.vue
@@ -42,10 +42,10 @@ defineProps<{
                   id="closeModal"
                   data-modal-hide="static-modal"
                   type="button"
-                  class="text-gray-500 bg-white hover:bg-white-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                  class="text-gray-500 bg-white hover:bg-white-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md border border-gray-200 text-md  px-5 py-3 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                   @click="$emit('close-modal')"
                 >
-                  Cancel
+                  Back
                 </button>
               </div>
             </div>
